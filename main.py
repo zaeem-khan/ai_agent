f2b5f1808f83d5751f6a90ee1d0753c976c8029e
zaeem-khan <hafizzaeemkhan@gmail.com>
2025-06-16 11:18:34 -0500

    Added prompt from CLI

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
+import sys
 
 
-model = "gemini-2.0-flash-001"
-prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-response = client.models.generate_content(model=model, contents=prompt)
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
 
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    client = genai.Client(api_key=api_key)
+
+    model = "gemini-2.0-flash-001"
+    try:
+        prompt = sys.argv[1]
+        response = client.models.generate_content(model=model, contents=prompt)
+    except IndexError:
+        print("Error: No Prompt Provided")
+        sys.exit(1)
+
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if __name__ == "__main__":
+    main()
