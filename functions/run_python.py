f73845616df14ed32d20059eac666184bd2784e8
zaeem-khan <hafizzaeemkhan@gmail.com>
2025-07-10 07:29:54 -0500

    run_python func

---
+++ functions/run_python.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path):
+    working_directory_abs = os.path.abspath(working_directory)
+    file_path_abs = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not file_path_abs.startswith(working_directory_abs):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+
+    if not os.path.exists(file_path_abs):
+        return f'Error: File "{file_path}" not found.'
+    
+    path_root, path_ext = os.path.splitext(file_path_abs)
+    if path_ext != '.py':
+        return f'Error: "{file_path}" is not a Python file.'
+    
+    try:
+        result = subprocess.run(['python', file_path_abs], capture_output=True, cwd=working_directory_abs, timeout=30)
+
+        if result.returncode != 0:
+            return f'Process exited with code {result.returncode}'
+        if result.stdout == '':
+            return 'No output produced'
+        output = f'STDOUT: {result.stdout}'
+        if result.stderr:
+            output += f'\nSTDERR: {result.stderr}'
+        return output
+    except Exception as e:
+        return f'Error: {e}'
--- tests.py
+++ tests.py
@@ -1,15 +1,19 @@
-from functions.write_file_content import write_file
+from functions.run_python import run_python_file
 
 print("Test 1")
-result_one = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+result_one = run_python_file("calculator", "main.py")
 print(result_one)
 
 print("Test 2")
-result_two = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+result_two = run_python_file("calculator", "tests.py")
 print(result_two)
 
 print("Test 3")
-result_three = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+result_three = run_python_file("calculator", "../main.py")
 print(result_three)
+
+print("Test 4")
+result_four = run_python_file("calculator", "nonexistent.py")
+print(result_four)
 
 
